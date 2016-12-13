9b45ce03c650774361c7d2cca5884bd2fdba4b45
Joanna Anderson and Sandro Alvarez <joanna+sandro@>
2016-12-13 11:06:16 -0800

    add Task model

---
+++ app/src/main/java/com/example/guest/organizer/models/Task.java
@@ -0,0 +1,88 @@
+package com.example.guest.organizer.models;
+
+/**
+ * Created by Guest on 12/13/16.
+ */
+public class Task {
+    String detail;
+    String type;
+    String signifier;
+    String status;
+    String day;
+    String month;
+    String collection;
+    String year;
+    String pushId;
+
+    public Task(){}
+
+    public Task(String detail, String type) {
+        this.detail = detail;
+        this.type = type;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getSignifier() {
+        return signifier;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getPushId() {
+        return pushId;
+    }
+
+    public String getCollection() {
+        return collection;
+    }
+
+    public void setSignifier(String signifier) {
+        this.signifier = signifier;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public void setPushId(String pushId) {
+        this.pushId = pushId;
+    }
+
+    public void setCollection(String collection) {
+        this.collection = collection;
+    }
+}
+
--- app/src/main/java/com/example/guest/organizer/ui/CreateUserActivity.java
+++ app/src/main/java/com/example/guest/organizer/ui/CreateUserActivity.java
@@ -1,13 +1,15 @@
-package com.example.guest.organizer;
+package com.example.guest.organizer.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.guest.organizer.R;
+
+public class CreateUserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_user);
     }
 }
